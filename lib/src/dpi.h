2ed750cda2c6572db26d0b72383ef2729427d284
Anthony Beaumont <dev.xan105@outlook.be>
2022-03-22 12:18:31 +0700

    license

--- lib/src/dpi.h
+++ lib/src/dpi.h
@@ -1,1 +1,7 @@
+/*
+Copyright (c) Anthony Beaumont
+This source code is licensed under the MIT License
+found in the LICENSE file in the root directory of this source tree.
+*/
+
 void DpiAwareness();
